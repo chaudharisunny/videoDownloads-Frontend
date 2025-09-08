4e310db7dfd234cc3a2c6203a2761f420fa78e5b
chaudhari sunny <chaudharisunny6@gmail.com>
2025-09-08 10:45:38 +0530

    update code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    (import.meta.env.DEV
+      ? "http://localhost:3000/api/v1"
+      : "https://your-backend.onrender.com/api/v1"),
 });
 
 export default api;
